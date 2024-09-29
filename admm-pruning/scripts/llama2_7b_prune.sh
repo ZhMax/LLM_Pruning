cfe96133a497bf832a7187433657b43bd2374b5e
m.zhelnin <m.zhelnin@an02.zhores>
2024-09-29 11:22:12 +0300

    admm update

--- admm-pruning/scripts/llama2_7b_prune.sh
+++ admm-pruning/scripts/llama2_7b_prune.sh
@@ -5,5 +5,6 @@ python /home/Quantization/admm-pruning/main.py \
     --prune_method admm \
     --sparsity_ratio 0.5 \
     --sparsity_type unstructured \
+    --nsamples 32000 \
     --save /home/exp_results/admm_pruning/unstructured/llama2_7b_claq3bit_admm_0@50 \
     --save_model /home/exp_results/admm_pruning/unstructured/llama2_7b_claq3bit_admm_0@50
