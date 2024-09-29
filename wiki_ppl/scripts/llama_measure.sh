aaada73b8bab4caa509b0e5d58548fe53ad046f0
m.zhelnin <m.zhelnin@an02.zhores>
2024-09-29 11:22:33 +0300

    wiki ppl 2048

--- wiki_ppl/scripts/llama_measure.sh
+++ wiki_ppl/scripts/llama_measure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python /home/LLM_Pruning/wiki_ppl/main.py \
-    --model /home/LLaMA/huggingface/Llama-2-7b-hf
+    --model /home/exp_results/sparseml/tutorial/pruning/llama2_7b_orig_sparse_wikitext2_2@4/stage_pruning
